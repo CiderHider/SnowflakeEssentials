91a141fddf75a23543fbcaeba8c5f304b5fafbd7
Aaron Brändli <a.braendli@saracus.com>
2023-01-20 09:04:36 +0100

    Added Dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 
 st.title('🥣🥗🐔 This is ma title🥑🍞')
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
 st.text('Hard-Boiled Free-Range Egg')
+
+st.dataframe(my_fruit_list)
