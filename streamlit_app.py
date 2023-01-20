4d0e934b2852abd425c5e5e0a3a277e532b6e351
Aaron Brändli <a.braendli@saracus.com>
2023-01-20 09:00:14 +0100

    Emojies wtf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title('This is ma title')
+st.title('🥣🥗🐔 This is ma title🥑🍞')
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberry Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
